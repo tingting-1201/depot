bef878d183b182f6433d68df82b2b93c2d3ba0f4
tingting-1201 <tingya1201@163.com>
2015-01-16 15:34:23 +0800

    XX

---
+++ app/models/line_item.rb
@@ -0,0 +1,14 @@
+class LineItem < ActiveRecord::Base
+  attr_accessible :cart_id, :product_id
+  attr_accessible :product
+
+  belongs_to :order
+  belongs_to :product
+  belongs_to :cart
+
+  def total_price
+    product.price * quantity
+  end
+
+
+end
